c10ac6da0548f44ed1176378223b39fac99e1228
John Milner <johnmilnerapple@gmail.com>
2021-03-25 19:53:23 -0700

    add FeatureBox component

---
+++ src/components/FeatureBox.js
@@ -0,0 +1,44 @@
+import "../vendor/icon-font.css";
+import React from "react";
+
+const FeatureBox = () => {
+  const renderedContent = [
+    {
+      icon: "world",
+      tag: "Explore the World",
+      copy:
+        "Hello world, dolor sit amet consectetur adipisicing elit. nesciunt nesciunt vitae",
+    },
+    {
+      icon: "compass",
+      tag: "Meet Nature",
+      copy:
+        "Heyyyy, dolor sit amet consectetur adipisicing elit. nesciunt nesciunt vitae",
+    },
+    {
+      icon: "map",
+      tag: "Find Your Way",
+      copy:
+        "Yowzer, dolor sit amet consectetur adipisicing elit. nesciunt nesciunt vitae",
+    },
+    {
+      icon: "heart",
+      tag: "Live a Healthier Life",
+      copy:
+        "Awesome, dolor sit amet consectetur adipisicing elit. nesciunt nesciunt vitae",
+    },
+  ];
+  return renderedContent.map((item) => {
+    return (
+      <div class="col-1-of-4">
+        <div class="feature-box">
+          <i class={`feature-box__icon icon-basic-${item.icon}`}></i>
+          <h3 class="heading-tertiary u-margin-bottom-small">{item.tag}</h3>
+          <p class="feature-box__text">{item.copy}</p>
+        </div>
+      </div>
+    );
+  });
+};
+
+export default FeatureBox;
